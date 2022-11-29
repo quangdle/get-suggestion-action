67a3f386b2aa3e66bb65f66d89e2228755437b17
Quang Le <quang.ledinh@employmenthero.com>
2022-11-29 16:08:55 +0700

    readfile

--- dist/index.js
+++ dist/index.js
@@ -9686,16 +9686,28 @@ var __webpack_exports__ = {};
 (() => {
 const core = __nccwpck_require__(1597);
 const github = __nccwpck_require__(566);
+const fs = __nccwpck_require__(7147);
 
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput("who-to-greet");
+  const repoPath = core.getInput("main_schema_path");
   console.log(`Hello ${nameToGreet}!`);
   const time = new Date().toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
+
+  const filenames = fs.readdirSync(
+    repoPath +
+      "/src/modules/ats/components/AtsJobDetailPage/MailIntegrationModal.js"
+  );
+
+  console.log("\nCurrent directory filenames:");
+  filenames.forEach((file) => {
+    console.log(file);
+  });
 } catch (error) {
   core.setFailed(error.message);
 }
--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const fs = require("fs");
 
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput("who-to-greet");
+  const repoPath = core.getInput("main_schema_path");
   console.log(`Hello ${nameToGreet}!`);
   const time = new Date().toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
+
+  const filenames = fs.readdirSync(
+    repoPath +
+      "/src/modules/ats/components/AtsJobDetailPage/MailIntegrationModal.js"
+  );
+
+  console.log("\nCurrent directory filenames:");
+  filenames.forEach((file) => {
+    console.log(file);
+  });
 } catch (error) {
   core.setFailed(error.message);
 }
